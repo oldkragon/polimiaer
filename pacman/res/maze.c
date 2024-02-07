51c426aae6d9372c189cd6d6efbbd198407936db
evasi <eva.sitzia@gmail.com>
2024-02-07 17:31:02 +0100

    Aggiunto un labirinto

---
+++ pacman/res/maze.c
@@ -0,0 +1,36 @@
+#define WIDTH 28
+#define HEIGHT 31
+
+
+    char maze[HEIGHT][WIDTH] = {{m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m}
+            {m, ., ., ., ., ., ., ., ., ., ., ., ., m, m, ., ., ., ., ., ., ., ., ., ., ., ., m}
+            {m, ., m, m, m, m, ., m, m, m, m, m, ., m, m, ., m, m, m, m, m, ., m, m, m, m, ., m}
+            {m, ., m, m, m, m, ., m, m, m, m, m, ., m, m, ., m, m, m, m, m, ., m, m, m, m, ., m}
+            {m, ., m, m, m, m, ., m, m, m, m, m, ., m, m, ., m, m, m, m, m, ., m, m, m, m, ., m}
+            {m, ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., m}
+            {m, ., m, m, m, m, ., m, m, ., m, m, m, m, m, m, m, m, ., m, m, ., m, m, m, m, ., m}
+            {m, ., m, m, m, m, ., m, m, ., m, m, m, m, m, m, m, m, ., m, m, ., m, m, m, m, ., m}
+            {m, ., ., ., ., ., ., m, m, ., ., ., ., m, m, ., ., ., ., m, m, ., ., ., ., ., ., m}
+            {m, m, m, m, m, m, ., m, m, m, m, m, ., m, m, ., m, m, m, m, m, ., m, m, m, m, m, m}
+            {m, m, m, m, m, m, ., m, m, m, m, m, ., m, m, ., m, m, m, m, m, ., m, m, m, m, m, m}
+            {m, m, m, m, m, m, ., m, m, ., ., ., ., ., ., ., ., ., ., m, m, ., m, m, m, m, m, m}
+            {m, m, m, m, m, m, ., m, m, ., m, m, m, ., ., m, m, m, ., m, m, ., m, m, m, m, m, m}
+            {m, m, m, m, m, m, ., m, m, ., m, ., ., ., ., ., ., m, ., m, m, ., m, m, m, m, m, m}
+            {., ., ., ., ., ., ., ., ., ., m, ., ., ., ., ., ., m, ., ., ., ., ., ., ., ., ., .}
+            {m, m, m, m, m, m, ., m, m, ., m, ., ., ., ., ., ., m, ., m, m, ., m, m, m, m, m, m}
+            {m, m, m, m, m, m, ., m, m, ., m, m, m, m, m, m, m, m, ., m, m, ., m, m, m, m, m, m}
+            {m, m, m, m, m, m, ., m, m, ., ., ., ., ., ., ., ., ., ., m, m, ., m, m, m, m, m, m}
+            {m, m, m, m, m, m, ., m, m, ., m, m, m, m, m, m, m, m, ., m, m, ., m, m, m, m, m, m}
+            {m, m, m, m, m, m, ., m, m, ., m, m, m, m, m, m, m, m, ., m, m, ., m, m, m, m, m, m}
+            {m, ., ., ., ., ., ., ., ., ., ., ., ., m, m, ., ., ., ., ., ., ., ., ., ., ., ., m}
+            {m, ., m, m, m, m, ., m, m, m, m, m, ., m, m, ., m, m, m, m, m, ., m, m, m, m, ., m}
+            {m, ., m, m, m, m, ., m, m, m, m, m, ., m, m, ., m, m, m, m, m, ., m, m, m, m, ., m}
+            {m, ., ., ., m, m, ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., m, m, ., ., ., m}
+            {m, m, m, ., m, m, ., m, m, ., m, m, m, m, m, m, m, m, ., m, m, ., m, m, ., m, m, m}
+            {m, m, m, ., m, m, ., m, m, ., m, m, m, m, m, m, m, m, ., m, m, ., m, m, ., m, m, m}
+            {m, ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., ., m}
+            {m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m}
+
+    }
+
+
