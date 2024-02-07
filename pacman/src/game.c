2a3b5c57f4292c33b54f9f6f0a93b68496b3d4b2
evasi <eva.sitzia@gmail.com>
2024-02-07 18:13:23 +0100

    Aggiunto inizio codice

---
+++ pacman/src/game.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define WIDTH 28
+#define HEIGHT 31
+#define WALL 'm'
+#define EMPTY ' '
+#define DOT '.'
+#define PACMAN 'p'
+#define GHOST 'g'
+
+typedef struct {
+    int x;
+    int y;
+} position;
+
+typedef struct{
+    char maze[HEIGHT][WIDTH];
+    position Pinky;
+    position Blinky;
+    position Inky;
+    position Clyde;
+    position PacMan;
+    int lives;
+    int score;
+}game;
+
+void InitializeGame(game Game);
+void UpdateGame(game Game);
+
+int main(){
+    game Game;
+
+    InitializeGame(Game);
+
+    while (1){
+        UpdateGame(Game);
+        if (game.lives == 0)
+            break;
+    }
+    return 0;
+
+}
+
+void InitializeGame(game Game){
+    char InitialMaze = //inserire la matrice
+    Game.lives = 3;
+    Game.score = 0;
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            Game.maze[i][j] = InitialMaze
+        }
+    }
+
+    Game.Pinky.x == Game.maze[]
+}
+
+
