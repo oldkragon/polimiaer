9eff0323ad16dc974d86233493bb36eee040e2c5
Leonardo Risari <leonardorisari@gmail.com>
2024-02-07 12:15:44 +0100

    Trying to show a window, but failing

---
+++ pacman/include/game.h
@@ -0,0 +1,14 @@
+//
+// Created by olthe on 07/02/2024.
+//
+
+#ifndef POLIMIAER_GAME_H
+#define POLIMIAER_GAME_H
+
+#endif //POLIMIAER_GAME_H
+
+#pragma  once
+
+#include <stdio.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
---
+++ pacman/src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "SDL2/SDL.h"
+
+int main(int argc, char*argv[]) {
+    if(SDL_Init(SDL_INIT_VIDEO)<0) {
+        printf("Failed to initialize the SDL2 library\n");
+        return -1;
+    }
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, 0);
+    if(!window) {
+        printf("Failed to create window\n");
+        return -1;
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
