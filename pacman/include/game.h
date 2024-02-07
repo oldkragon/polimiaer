f2437e0170b68750585a65a53c9bfae170319104
Leonardo Risari <leonardorisari@gmail.com>
2024-02-07 19:25:31 +0100

    Started to implement game loop

--- pacman/include/game.h
+++ pacman/include/game.h
@@ -3,6 +3,7 @@
 #define POLIMIAER_GAME_H
 
 int ShowWindow();
+void ShowMaze(SDL_Renderer *renderer);
 
 #endif //POLIMIAER_GAME_H
 
--- pacman/src/main.c
+++ pacman/src/main.c
@@ -3,7 +3,32 @@
 #include "game.h"
 
 int SDL_main(int argc, char* argv[]) {
-    int err = ShowWindow();
-    printf("%d", err);
+    int running = 1;
+    if(SDL_Init(SDL_INIT_VIDEO)!=0) {
+        return -1;
+    }
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 600, SDL_WINDOW_SHOWN);
+    if(window==NULL) {
+        SDL_Quit();
+        return -1;
+    }
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(renderer==NULL) {
+        SDL_Quit();
+        return -1;
+    }
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+            ShowMaze(renderer);
+        }
+    }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
